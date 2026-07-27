c0b029ed1fe1c651e95dfad0bf7b2471fcd39e8c
DoloDood <149717459+DoloDood@users.noreply.github.com>
2026-07-26 20:40:39 -0400

    reset

--- app/page.js
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation';
-import { getCurrentUser } from '../lib/supabase/server.js';
-
-export default async function HomePage() {
-  const user = await getCurrentUser();
-  if (!user) redirect('/login');
-  if (!user.username) redirect('/onboarding');
-  redirect('/search');
-}
